317eb469e664a0f2f118478886b4a95094f7c6b2
Brian <bstanton773@gmail.com>
2023-04-03 14:43:28 -0500

    Add navbar with props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
+import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
+    let random = Math.floor((Math.random()*100));
     let name = 'Brian';
-  return (
-    <div className="App">
-        <h1>Hello {name}</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar username={name} city={"Chicago"}/>
+            <div className='container'>
+            {random > 50 ? <h1>Hello {name} {random}</h1> : <h1>Good Bye {name} {random}</h1>}
+            
+            <h4>Hello again</h4>
+            </div>
+        </div>
+    );
 }
 
 export default App;
