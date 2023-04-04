8633aafa54b28f84c4a53f06e1eb3690558a3baa
Brian <bstanton773@gmail.com>
2023-04-04 14:06:42 -0500

    Move button content to Button Display view component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Navbar from './components/Navbar';
-import Button from './components/Button';
+// import { Route } from 'react-router-dom';
+import ButtonDisplay from './views/ButtonDisplay';
 
 function App() {
     let name = 'Brian';
-    let buttons = [
-        {color: 'primary', step: 1},
-        {color: 'secondary', step: 10},
-        {color: 'success', step: 100},
-        {color: 'danger', step: 1000}
-    ];
-
-    const [count, setCount] = useState(0);
-
-    function increaseCount(step){
-        setCount(count + step)
-    };
 
     return (
         <div className="App">
             <Navbar username={name} city={"Chicago"}/>
             <div className='container'>
-                {count < 50 ? <h1>Hello {name} {count}</h1> : <h1>Good Bye {name} {count}</h1>}
-                {buttons.map(button => <Button key={button.step} color={button.color} step={button.step} handleClick={increaseCount}/> )}
+                <ButtonDisplay name={name} />
             </div>
         </div>
     );
