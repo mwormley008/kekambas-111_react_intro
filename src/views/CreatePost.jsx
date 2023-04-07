c9779bc92bdb4378d4625b51cb04c4600c5a4241
Brian <bstanton773@gmail.com>
2023-04-07 14:58:42 -0500

    Handle create post submite

--- src/views/CreatePost.jsx
+++ src/views/CreatePost.jsx
@@ -13,9 +13,39 @@ export default function CreatePost({ loggedIn, flashMessage }) {
         }
     })
 
-    function handleSubmit(e){
+    async function handleSubmit(e){
         e.preventDefault();
-        console.log(e);
+
+        // Get the data from the form
+        let title = e.target.title.value;
+        let body = e.target.body.value;
+
+        // Get the token from localStorage
+        let token = localStorage.getItem('token');
+
+        // Set up the request headers
+        let myHeaders = new Headers();
+        myHeaders.append('Content-Type', 'application/json');
+        myHeaders.append('Authorization', `Bearer ${token}`);
+
+        // Set up the request body
+        let requestBody = JSON.stringify({ title, body })
+
+        // Make the fetch request
+        let response = await fetch('http://localhost:5000/api/posts', {
+            method: 'POST',
+            headers: myHeaders,
+            body: requestBody
+        });
+
+        let data = await response.json();
+
+        if (data.error){
+            flashMessage(data.error, 'danger');
+        } else {
+            flashMessage(`${data.title} has been created`, 'primary');
+            navigate('/');
+        };
     };
 
     return (
